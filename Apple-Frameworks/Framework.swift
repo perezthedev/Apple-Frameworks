06121a1fe644d168416bcaf7ae54b6cc212c5bd8
Ryan Perez <ryancs115@gmail.com>
2021-08-16 20:11:03 -0700

    created Framework.swift file for Framework struct

---
+++ Apple-Frameworks/Framework.swift
@@ -0,0 +1,15 @@
+//
+//  Framework.swift
+//  Apple-Frameworks
+//
+//  Created by thepercussivedev on 8/16/21.
+//
+
+import SwiftUI
+
+struct Framework {
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+}
