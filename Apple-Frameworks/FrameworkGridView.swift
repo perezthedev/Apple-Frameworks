a27abb6bcca5515f9dd519596ff3b60d053f8e9b
Ryan Perez <ryancs115@gmail.com>
2021-08-17 05:12:40 -0700

    refactored to FrameworkTitleView struct

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -9,12 +9,30 @@ import SwiftUI
 
 struct FrameworkGridView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        FrameworkTitleView(name: "App Clips", imageName: "app-clip")
     }
 }
 
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+    }
+}
+
+struct FrameworkTitleView: View {
+    let name: String
+    let imageName: String
+    var body: some View{
+        VStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+            
+        }
     }
 }
