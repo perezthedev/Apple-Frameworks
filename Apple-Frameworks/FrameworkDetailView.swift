e8b1f79cbcca2c995ef9ca37de206604bf7b8866
Ryan Perez <ryancs115@gmail.com>
2021-08-21 17:04:46 -0700

    updated FrameworkDetailView with isShowingSafariView sheet

--- Apple-Frameworks/FrameworkDetailView.swift
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -11,6 +11,7 @@ struct FrameworkDetailView: View {
     
     var framework: Framework
     @Binding var isShowingDetailView: Bool
+    @State private var isShowingSafariView = false
     
     var body: some View {
         VStack {
@@ -37,12 +38,14 @@ struct FrameworkDetailView: View {
             Spacer()
             
             Button {
-                
+                isShowingSafariView = true
             } label: {
                AFButton(title: "Learn More")
-                   
             }
         }
+        .sheet(isPresented: $isShowingSafariView, content: {
+            SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
+        })
     }
 }
 
